d664f47d747f39540278ce8060080c07e44a3862
Amir HTH <uo240753@uniovi.es>
2018-12-02 15:40:26 +0100

    new menu

--- app/src/main/java/com/prototype/droid2car/droid2car/MainActivity.kt
+++ app/src/main/java/com/prototype/droid2car/droid2car/MainActivity.kt
@@ -95,24 +95,22 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
+            R.id.nav_ordenes -> {
                 // Handle the camera action
             }
-            R.id.nav_gallery -> {
+            R.id.nav_estancias -> {
 
             }
-            R.id.nav_slideshow -> {
+            R.id.nav_historico -> {
 
             }
-            R.id.nav_manage -> {
+            R.id.nav_configuracion -> {
 
             }
             R.id.nav_share -> {
 
             }
-            R.id.nav_send -> {
 
-            }
         }
 
         drawer_layout.closeDrawer(GravityCompat.START)
